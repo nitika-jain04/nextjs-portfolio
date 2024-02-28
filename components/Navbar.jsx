3a734c045326e23161b51e7419d529d2ec9a5aa6
Nitika Jain <nitikaaajain.04@gmail.com>
2024-02-28 19:29:03 +0530

    alignment and logic fixes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Navbar() {
+  const [clicked, setClicked] = useState(null);
+
+  function handleClick(link) {
+    setClicked(link);
+  }
+
   return (
     <div className="px-40 flex items-center justify-between py-5 border-b border-gray-400 drop-shadow-lg shadow-md">
       <div>
@@ -11,10 +18,42 @@ export default function Navbar() {
       </div>
 
       <div className="flex gap-5 items-center">
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/contact">Contact Me</Link>
+        <Link
+          href="/"
+          className={`p-2 text-[#111111] ${
+            clicked === "/" ? "bg-gray-300 rounded-lg" : ""
+          }`}
+          onClick={() => handleClick("/")}
+        >
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className={`p-2 text-[#111111] ${
+            clicked === "/about" ? "bg-gray-300 rounded-lg" : ""
+          }`}
+          onClick={() => handleClick("/about")}
+        >
+          About
+        </Link>
+        <Link
+          href="/projects"
+          className={`p-2 text-[#111111] ${
+            clicked === "/projects" ? "bg-gray-300 rounded-lg" : ""
+          }`}
+          onClick={() => handleClick("/projects")}
+        >
+          Projects
+        </Link>
+        <Link
+          href="/contact"
+          className={`p-2 text-[#111111] ${
+            clicked === "/contact" ? "bg-gray-300 rounded-lg" : ""
+          }`}
+          onClick={() => handleClick("/contact")}
+        >
+          Contact Me
+        </Link>
       </div>
     </div>
   );
